0b7909607c9f5c12857ab87841072129d3769a70
nadirEN <nadirelkhaldi@yahoo.fr>
2015-09-18 11:55:48 -0500

    Created by Coding Ground Client

---
+++ New Project/Variables.c
@@ -0,0 +1,5 @@
+unsigned short cpt0, cpt1, cpt2, ctrl_t;
+unsigned short ad, stat_ad,ctrl_ad, voie;
+unsigned short va_data,vb_data, conv_ad;
+unsigned short in_log, out_log;
+unsigned short int_st, int_en;
